30d55aa8175fd72197c4b11f042c81fdf1415f82
sreenivasn <sreenivas.narne@thoughtfocus.com>
2019-03-24 20:05:10 +0530

    2nd commit

--- src/test/java/ToolsQA/APITestingFramework/SecondTest.java
+++ src/test/java/ToolsQA/APITestingFramework/SecondTest.java
@@ -11,9 +11,15 @@ public class SecondTest {
 	}
 	
 	@Test
-	public void test2()
+	public void test4()
 	{
 		System.out.println("this is test 4");
+	}
+	
+	@Test
+	public void test5()
+	{
+		System.out.println("this is test 5");
 	}
 
 }

7ebeb43660e630511dcff848fab356ac85cca78f
sreenivasn <sreenivas.narne@thoughtfocus.com>
2019-03-25 19:59:46 +0530

    Log 4j commited

--- src/test/java/ToolsQA/APITestingFramework/FirstTest.java
+++ src/test/java/ToolsQA/APITestingFramework/FirstTest.java
@@ -1,19 +1,27 @@
 package ToolsQA.APITestingFramework;
 
+
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.testng.annotations.Test;
 
 public class FirstTest {
 	
+	private static Logger log=LogManager.getLogger(FirstTest.class.getName());
+	
 	@Test
 	public void test1()
 	{
-		System.out.println("this is test 1");
+		//System.out.println("this is test 1");
+		log.info("this is test 1");
 	}
 	
 	@Test
 	public void test2()
 	{
-		System.out.println("this is test 2");
+		//System.out.println("this is test 2");
+		log.info("this is test 2");
 	}
 
 }
